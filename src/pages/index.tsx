3db6b04a457ee61f48720401efe7eb48ecc6e0dd
Ricky L. T. Wong <rickyltwong2+git@gmail.com>
2024-02-28 19:44:20 -0500

    Revert "Side Menu Component"
    This reverts commit 47e9783de028736f29b2e6c6abdbba9acaf12201.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,35 @@
+import Head from 'next/head'
 import Dashboard from "@/pages/dashboard";
+import SideMenu from "@/components/SideMenu";
 import Login from "@/components/Login";
-import { useSession } from "next-auth/react";
-import scss from "./Home.module.scss";
+import {useSession} from "next-auth/react";
+import scss from './Home.module.scss';
 import React from "react";
 
 const Home: React.FC = () => {
-  const { data: session } = useSession();
+    const { data: session } = useSession();
 
-  return (
-    <>
-      <main className={scss.main}>
-        {session && <Dashboard />}
-        {!session && <Login />}
-      </main>
-    </>
-  );
-};
+    return (
+        <>
+            <Head>
+                <title>DataSoft - Dashboard</title>
+                <meta name="description" content="Data Dashboard" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={scss.main}>
+                {
+                    session && (
+                        <>
+                            <SideMenu />
+                            <Dashboard />
+                        </>
+                    )
+                }
+                <Login />
+            </main>
+        </>
+    )
+}
 
 export default Home;
